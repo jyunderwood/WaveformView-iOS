1a4038d9270f832bda04c6c4eaf5e6f090973ff1
Jonathan Underwood <jonathan@jyunderwood.com>
2016-03-24 10:21:35 -0500

    Upgrade to Xcode 7.3 and swift 2.2 syntax

--- SiriWaveformView/SiriWaveformView.swift
+++ SiriWaveformView/SiriWaveformView.swift
@@ -12,12 +12,11 @@ import Darwin
 let pi = M_PI
 
 @IBDesignable
-public class SiriWaveformView: UIView
-{
+public class SiriWaveformView: UIView {
     private var _phase: CGFloat = 0.0
     private var _amplitude: CGFloat = 0.0
 
-    @IBInspectable public var waveColor = UIColor.whiteColor()
+    @IBInspectable public var waveColor: UIColor = UIColor.blackColor()
     @IBInspectable public var numberOfWaves = 5
     @IBInspectable public var primaryWaveLineWidth: CGFloat = 3.0
     @IBInspectable public var secondaryWaveLineWidth: CGFloat = 1.0
@@ -39,46 +38,49 @@ public class SiriWaveformView: UIView
     }
 
     override public func drawRect(rect: CGRect) {
-        var context = UIGraphicsGetCurrentContext()
+        let context = UIGraphicsGetCurrentContext()
         CGContextClearRect(context, bounds)
 
         backgroundColor?.set()
         CGContextFillRect(context, rect)
 
-        // We draw multiple sinus waves, with equal phases but altered
+        // Draw multiple sinus waves, with equal phases but altered
         // amplitudes, multiplied by a parable function.
-        for var i = 0; i < numberOfWaves; i++ {
-            var context = UIGraphicsGetCurrentContext()
+        for waveNumber in 0...numberOfWaves {
+            let context = UIGraphicsGetCurrentContext()
 
-            CGContextSetLineWidth(context, (i == 0 ? primaryWaveLineWidth : secondaryWaveLineWidth))
+            CGContextSetLineWidth(context, (waveNumber == 0 ? primaryWaveLineWidth : secondaryWaveLineWidth))
 
-            var halfHeight = CGRectGetHeight(bounds) / 2.0
-            var width = CGRectGetWidth(bounds)
-            var mid = width / 2.0
+            let halfHeight = CGRectGetHeight(bounds) / 2.0
+            let width = CGRectGetWidth(bounds)
+            let mid = width / 2.0
 
             let maxAmplitude = halfHeight - 4.0 // 4 corresponds to twice the stroke width
 
             // Progress is a value between 1.0 and -0.5, determined by the current wave idx, which is used to alter the wave's amplitude.
-            var progress = CGFloat(1.0 - Float(i) / Float(numberOfWaves))
-            var normedAmplitude = (1.5 * progress - 0.5) * amplitude
+            let progress: CGFloat = 1.0 - CGFloat(waveNumber) / CGFloat(numberOfWaves)
+            let normedAmplitude = (1.5 * progress - 0.5) * amplitude
 
-            var multiplier = CGFloat(min(1.0, (progress / 3.0 * 2.0) + (1.0 / 3.0)))
+            let multiplier: CGFloat = 1.0
             waveColor.colorWithAlphaComponent(multiplier * CGColorGetAlpha(waveColor.CGColor)).set()
 
-            for var x: CGFloat = 0.0; x < width + density; x += density {
-                // We use a parable to scale the sinus wave, that has its peak in the middle of the view.
-                var scaling = -pow(1 / mid * (x - mid), 2) + 1
+            var x: CGFloat = 0.0
+            while x < width + density {
+                // Use a parable to scale the sinus wave, that has its peak in the middle of the view.
+                let scaling = -pow(1 / mid * (x - mid), 2) + 1
 
                 // Original Code:
                 // CGFloat y = scaling * maxAmplitude * normedAmplitude * sinf(2 * M_PI *(x / width) * self.frequency + self.phase) + halfHeight;
-                var tempCasting = CGFloat(2.0 * Float(pi) * Float(x / width)) * frequency + _phase
-                var y = scaling * maxAmplitude * normedAmplitude * CGFloat(sinf(Float(tempCasting))) + halfHeight
+                let tempCasting: CGFloat = 2.0 * CGFloat(pi) * CGFloat(x / width) * frequency + _phase
+                let y = scaling * maxAmplitude * normedAmplitude * CGFloat(sinf(Float(tempCasting))) + halfHeight
 
                 if x == 0 {
                     CGContextMoveToPoint(context, x, y)
                 } else {
                     CGContextAddLineToPoint(context, x, y)
                 }
+
+                x += density
             }
 
             CGContextStrokePath(context)
